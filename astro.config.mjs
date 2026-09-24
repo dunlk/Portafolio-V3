49297b3d02cd16a8726b726441f9ffac41e64105
Milton Ytusaca <milton.yv98@gmail.com>
2026-09-23 22:19:11 -0500

    chore: organizacion de archivos

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  devToolbar: { enabled: false },
+  // devToolbar: { enabled: false },
   vite: {
     plugins: [tailwindcss()],
   },
 });
-
